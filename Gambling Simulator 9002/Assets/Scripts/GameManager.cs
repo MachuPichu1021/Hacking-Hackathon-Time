4e87f1144e217338c0b47687d219bc7d00a7c652
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2025-02-08 22:56:38 -0500

    Update GameManager.cs

--- Gambling Simulator 9002/Assets/Scripts/GameManager.cs
+++ Gambling Simulator 9002/Assets/Scripts/GameManager.cs
@@ -16,4 +16,34 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void LegDebuff(float speed)
+    {
+        speed /= 2;
+    }
+
+    public void HandDebuff(float horizontalInput, float verticalInput)
+    {
+        horizontalInput = Input.GetAxisRaw("Vertical");
+        verticalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    public void KidneyDebuff()
+    {
+        StartCoroutine(Shake());
+    }
+
+    private IEnumerator Shake()
+    {
+        float shakeDuration = 3f;
+        float shakeMagnitude = 75f;
+        
+        while (shakeDuration > 0)
+        {
+            shakeDuration -= Time.deltaTime;
+            Vector2 point = Random.insideUnitCircle * shakeMagnitude;
+            GetComponent<Camera>().transform.localPosition = Vector3.Lerp(GetComponent<Camera>().transform.localPosition, point, Time.deltaTime);
+            yield return null;
+        }
+    }
 }
